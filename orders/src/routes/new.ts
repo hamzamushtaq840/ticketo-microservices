aa85d083a7475d289cd09d85b0afa96c81031493
Hamza Mushtaq <73603757+hamzamushtaq840@users.noreply.github.com>
2024-06-26 14:47:10 +0500

    feat: listener config

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -61,6 +61,7 @@ router.post(
       id: order.id,
       status: order.status,
       userId: order.userId,
+      version: 2,
       expiresAt: order.expiresAt.toISOString(),
       ticket: {
         id: ticket.id,
